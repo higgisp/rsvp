8288ace915f94221ddef881416bfdfb6c74fbc74
Samuel Higginbotham <sam@isotope11.com>
2013-06-29 03:12:59 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsvp::Application.config.secret_token = 'b97a4cbde83b530499d15dda7a74e7805b5c96af2bb0a99edd35aaa4d8fa31aa3038ff5d01c03f2d32665e3068456d906a65e752ccf401aabc86d6ff873dc57d'
